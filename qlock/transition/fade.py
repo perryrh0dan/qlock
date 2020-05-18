1b9ff7b69ff3e6a9a8ae35cc8aad692f8d956ef0
perryrh0dan <thomaspoehlmann96@googlemail.com>
2020-05-18 15:30:23 +0200

    feat(fade): add fade effect

--- qlock/transition/fade.py
+++ qlock/transition/fade.py
@@ -3,22 +3,22 @@ import numpy as np
 
 def start(ctrl, old_word_leds, new_word_leds):
     # Fade new words in
-    led = old_word_leds + new_word_leds
-    colors = np.tile(np.array(ctrl.color), (len(old_word_leds), 1))
+    led = new_word_leds + old_word_leds
+    original_colors = np.tile(np.array(ctrl.color), (len(old_word_leds), 1))
     for i in range(10):
         i += 1
         color = fade(ctrl.color, (i / 10))
-        colors = np.append(colors, np.tile(np.array(color), (len(new_word_leds), 1)), axis = 0)
+        colors = np.append(np.tile(np.array(color), (len(new_word_leds), 1)), original_colors, axis = 0)
         ctrl.set_pixels(led, colors)
         time.sleep(0.05)
 
     # Fade old words out
-    led = new_word_leds + old_word_leds
-    colors = np.tile(np.array(ctrl.color), (len(old_word_leds), 1))
+    led = old_word_leds + new_word_leds
+    original_colors = np.tile(np.array(ctrl.color), (len(old_word_leds), 1))
     for i in range(10):
         i += 1
         color = fade(ctrl.color, (1 - i/10))
-        colors = np.append(colors, np.tile(np.array(color), (len(new_word_leds), 1)), axis = 0)
+        colors = np.append(np.tile(np.array(color), (len(new_word_leds), 1)), original_colors, axis = 0)
         ctrl.set_pixels(led, colors)
         time.sleep(0.05)
 
